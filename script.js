21289448a57bf30f138576ba40af713019490f8a
vynux <contact@clacs.io>
2024-12-01 16:49:35 +0100

    script.js

--- script.js
+++ script.js
@@ -113,7 +113,7 @@ if (!ext.supportLinearFiltering) {
     config.SUNRAYS = false;
 }
 
-startGUI();
+// startGUI();
 
 function getWebGLContext (canvas) {
     const params = { alpha: true, depth: false, stencil: false, antialias: false, preserveDrawingBuffer: false };
